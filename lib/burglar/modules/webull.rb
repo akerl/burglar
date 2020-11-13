4a99c482c6ad7cc606ccf13cf4fb998e90989dec
Les Aker <me@lesaker.org>
2020-11-13 16:00:05 -0500

    update webull logic

--- lib/burglar/modules/webull.rb
+++ lib/burglar/modules/webull.rb
@@ -11,41 +11,66 @@ module LogCabin
       WEBULL_DOMAIN = 'https://webull.com'
 
       def raw_transactions
-        @raw_transactions ||= all_transactions.map do |row|
+        @raw_transactions ||= filtered_orders.map do |row|
           ::Ledger::Entry.new(
-            name: name,
+            name: "#{row[:action]} #{,
             state: :cleared,
-            date: row.date,
+            date: row[:date],
             actions: [
               { name: action, amount: amount },
               { name: account_name }
             ],
-            tags: { 'transaction_id' => row.transaction_id }
+            tags: { 'transaction_id' => row[:id] }
           )
         end
       end
 
       private
 
+      def all_events
+        @all_events ||= api_client.orders(status: 'Filled', pageSize: 999)
+      end
 
-      def all_transactions
-        @all_transactions = api_client.transactions(
-          after: begin_date,
-          before: end_date,
-          type: 'filled'
-        )
+      def all_orders
+        @all_orders ||= all_events.map { |x| x.orders }.flatten
+      end
+
+      def parsed_orders
+        @parsed_orders ||= all_orders.map do |x|
+          {
+            id: x['orderId']
+            symbol: x['symbol'],
+            action: x['action'],
+            quantity: x['filledQuantity'],
+            date: Time.at(x['filledTime0'] / 1000).to_date,
+            total: x['filledValue'],
+            price: x['avgFilledPrice']
+          }
+        end
+      end
+
+      def filtered_orders
+        @filtered_orders ||= parsed_orders.select do |x|
+          x['date'] >= begin_date && x['date'] <= end_date
+        end
       end
 
       def api_client
         return @api_client if @api_client
-        generate_tokens unless refresh_token && access_token
-        @api_client = ::Webull.new(refresh_token, access_token)
-        store_tokens @api_client.refresh_token
+        @api_client = ::Webull.new(tokens: tokens, udid: udid)
+        store_tokens @api_client.refresh
         @api_client
       end
 
-      def generate_tokens
-        store_tokens Webull.generate_tokens
+      def tokens
+        @tokens ||= if access_token && refresh_token
+                      ::Webull::Tokens.new(
+                        access: access_token,
+                        refresh: refresh_token
+                      )
+                    else
+                      ::Webull.generate_tokens(udid)
+                    end
       end
 
       def store_tokens(tokens)
@@ -59,6 +84,10 @@ module LogCabin
 
       def access_token
         @access_token ||= stored_creds(WEBULL_DOMAIN, 'access_token')
+      end
+
+      def udid
+        @udid ||= @options[:device_name] || raise('Must supply a device_name')
       end
     end
   end
